ae3b833c5e854798a4a354e04388581b269e0734
Adam LaCasse <adamlacasse@outlook.com>
2018-06-04 16:56:25 -0400

    cleaned up, working minimum requirements

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,63 +21,74 @@ function validateInput(value) {
 	}
 }
 
-function promptUserPurchase() {
-	inquirer.prompt([
+function chooseItem(){
+		inquirer.prompt([
 		{
-			type: 'input',
-			name: 'item_id',
-			message: 'Please enter the Product # for the product you would like to purchase.\n\n',
-			validate: validateInput,
-			filter: Number
-		},
-		{
-			type: 'input',
-			name: 'quantity',
-			message: 'How many do you need?',
-			validate: validateInput,
-			filter: Number
+		type: 'input',
+		name: 'item_id',
+		message: 'Please enter the Product # for the product you would like to purchase.\n\n',
+		validate: validateInput,
+		filter: Number
 		}
-	]).then(function(input) {
-
+	]).then(function(input){
 		var item = input.item_id;
-		var quantity = input.quantity;
-
 		var queryStr = 'SELECT * FROM products WHERE ?';
 
-		con.query(queryStr, {item_id: item}, function(err, data) {
+		con.query(queryStr, {item_id: item}, function(err, data){
 			if (err) throw err;
-
 			if (data.length === 0) {
 				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
 				displayInventory();
-
-			} else {
-				var productData = data[0];
-
-				if (quantity <= productData.stock_quantity) {
-					console.log('Congratulations, the product you requested is in stock! Placing order!');
-
-					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-
-					con.query(updateQueryStr, function(err, data) {
-						if (err) throw err;
-
-						console.log('Your oder has been placed! Your total is $' + productData.consumer_price * quantity);
-						console.log('Thank you for shopping with us!');
-						console.log("\n---------------------------------------------------------------------\n");
-
-						con.end();
-					})
-				} else {
-					console.log('Sorry, there is not enough product in stock.');
-					console.log('Please modify your order keeping in mind the Qty Available.');
-					console.log("\n---------------------------------------------------------------------\n");
-					promptUserPurchase();
-				}
 			}
-		})
-	})
-} // end of promptUserPurchase
+			else {
+				chooseQty(item);
+			}
+		});  // end of con.query
+	}); // end of .then
+} // end of chooseItem
+
+
+function chooseQty(qty){
+	inquirer.prompt([
+	{
+	type: 'input',
+	name: 'quantity',
+	message: 'How many do you need?\n\n',
+	validate: validateInput,
+	filter: Number
+	}
+]).then(function(input){
+	var item = qty;
+	var quantity = input.quantity;
+	var queryStr = 'SELECT * FROM products WHERE ?';
+
+	con.query(queryStr, {item_id: item}, function(err, data){
+		if (err) throw err;
+		var productData = data[0];
+		if (quantity <= productData.stock_quantity){
+			console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+			var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+			con.query(updateQueryStr, function(err, data) {
+				if (err) throw err;
+
+				console.log('Your oder has been placed! Your total is $' + productData.consumer_price * quantity);
+				console.log('Thank you for shopping with us!');
+				console.log("\n---------------------------------------------------------------------\n");
+
+				con.end();
+			}); // end of con.query
+		} // end of if (quantity <= productData.stock_quantity)
+		else {
+			console.log('Sorry, there is not enough product in stock.');
+			console.log('Please modify your order keeping in mind the Qty Available.');
+			console.log("\n---------------------------------------------------------------------\n");
+			chooseItem();
+		} // end of else
+	});  // end of con.query
+	}); // end of .then
+} // end of chooseQty
 
 function displayInventory(){
     con.query('SELECT item_id, product_name, department_name, consumer_price, stock_quantity FROM products', function (error, results) {
@@ -88,12 +99,10 @@ function displayInventory(){
           	displayString += `Name: ${results[i].product_name} | `;
           	displayString += `Price: $${results[i].consumer_price} | `;
 		  	displayString += `Qty Available: ${results[i].stock_quantity}\n\n`;
-          	// displayString += '-----------------------------------------\n'
         }
         console.log(displayString);
 	});
-	promptUserPurchase();
+	chooseItem();
   } // end of displayInventory
 
 displayInventory();
-// promptUserPurchase();
